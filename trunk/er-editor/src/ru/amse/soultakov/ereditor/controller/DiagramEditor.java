1aa452b7e2982419430bd530e6712a0e7a00a441
soultakov <soultakov@4157c926-ea29-0410-bc01-7ddc54ad91d9>
2007-02-21 14:26:56 +0000

    Some unimportant experiments with DiagramEditor class.

--- trunk/er-editor/src/ru/amse/soultakov/ereditor/controller/DiagramEditor.java
+++ trunk/er-editor/src/ru/amse/soultakov/ereditor/controller/DiagramEditor.java
@@ -3,6 +3,10 @@
  */
 package ru.amse.soultakov.ereditor.controller;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 import javax.swing.JComponent;
 
 /**
@@ -11,5 +15,25 @@ import javax.swing.JComponent;
  */
 public class DiagramEditor extends JComponent
 {
+
+	public DiagramEditor()
+	{
+	}
 	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(getX(), getY(), getWidth(), getHeight());		
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.swing.JComponent#getPreferredSize()
+	 */
+	@Override
+	public Dimension getPreferredSize()
+	{
+		return new Dimension(200,100);
+	}
 }
