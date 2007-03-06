6f4f55f33f713c9c4d9ec395caf5a67961617fef
soultakov <soultakov@4157c926-ea29-0410-bc01-7ddc54ad91d9>
2007-03-06 06:51:06 +0000

    Another tests added

---
+++ trunk/er-editor/tst/ru/amse/soultakov/ereditor/model/CommentTest.java
@@ -0,0 +1,56 @@
+/*
+ * Created on 06.03.2007
+ */
+package ru.amse.soultakov.ereditor.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+public class CommentTest {
+
+    @Test
+    public void testComment() {
+        Comment comment = new Comment("comment");
+        assertEquals(comment.getComment(), "comment");
+    }
+
+    @Test
+    public void testSetComment() {
+        Comment comment = new Comment("comment");
+        comment.setComment("comment1");
+        assertEquals(comment.getComment(), "comment1");
+    }
+
+    @Test
+    public void testAddLink() {
+        Comment comment = new Comment("comment");
+        Link link = createLink(comment);
+        assertTrue(hasLink(comment, link));
+    }
+    
+    private boolean hasLink(Comment comment, Link link) {
+        for (Iterator<Link> i = comment.linksIterator(); i.hasNext();) {
+            if (i.next().equals(link)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private static Link createLink(Comment comment) {
+        final Entity entity = new Entity("");
+        return new Link(entity, comment);
+    }
+
+    @Test
+    public void testRemoveLink() {
+        Comment comment = new Comment("comment");
+        Link link = createLink(comment);
+        comment.removeLink(link);
+        assertFalse(hasLink(comment, link));
+    }
+
+}
